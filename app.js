79b3e0249179fda073b1cf7e79de886aa1ba6fe9
FernandoEtcheverry <francisco.etcheverry@cencosud>
2022-09-09 17:21:54 -0300

    quedo para todo menos .ejs

--- app.js
+++ app.js
@@ -2,17 +2,17 @@
 const express = require("express");
 const app = express();
 // Acá falta uno... 😇
-const routes = require("./routes/mainRouter");
+const routes = require("./src/routes/mainRouter");
 // Configuración
 app.use(express.static("public"));
 // Acá falta el template engine
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 // Rutas
 // Acá falta el archivo de rutas y después las vistas de EJS
 app.use("/", routes);
 
-app.listen(3000, () => {
-  console.log("Servidor arriba en el puerto 3000 🤓👌");
+app.listen(5000, () => {
+  console.log("Servidor arriba en el puerto 5000 🤓👌");
 });
