c3f5d2aa810ff029976c2462cb963bb29792f091
facundovacanicencosud <facundo.vacani@cencosud.com>
2022-09-10 15:10:57 -0300

    add images in the menus and array

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,24 +5,28 @@ let platos = [
     nombre: "Carpaccio fresco",
     descripcion: "Entrada Carpaccio de salmón con cítricos",
     precio: 65.5,
+    img: "/images/Carpaccio-de-salmon.jpg"
   },
   {
     id: 2,
     nombre: "Risotto de berenjena",
     descripcion: "Risotto de berenjena y queso de cabra",
     precio: 47.0,
+    img: "/images/Risotto-berenjena-queso-cabra.jpg"
   },
   {
     id: 3,
     nombre: "Mouse de arroz",
     descripcion: "Mouse de arroz con leche y aroma de azahar",
     precio: 27.5,
+    img: "/images/Mousse-de-arroz-con-leche.jpg"
   },
   {
     id: 4,
     nombre: "Espárragos blancos",
     descripcion: "Espárragos blancos con vinagreta de verduras y jamón ibérico",
     precio: 37.5,
+    img: "/images/esparragos.png"
   },
 ];
 
